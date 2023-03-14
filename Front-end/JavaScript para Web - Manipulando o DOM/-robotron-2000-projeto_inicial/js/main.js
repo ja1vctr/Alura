cb96588d8ab7c246d765b899ef7c91dee357c06f
joaovictor <jaovictor3690@gmail.com>
2023-03-14 17:59:12 -0300

    Adicionando arquivos do novo curso de JS

---
+++ Front-end/JavaScript para Web - Manipulando o DOM/-robotron-2000-projeto_inicial/js/main.js
@@ -0,0 +1,10 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", (evento) => {
+    console.log(evento)
+})
+
+function dizOi() {
+    console.log("oi")
+    console.log("Bem-vindo ao Robotron 2000")
+}
