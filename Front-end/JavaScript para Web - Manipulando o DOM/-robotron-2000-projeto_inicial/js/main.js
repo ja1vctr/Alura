650059aee89f7b3a98de0a1c69345327b6a7dbd3
joaovictor <jaovictor3690@gmail.com>
2023-03-22 14:05:11 -0300

    adicionando novas alterações no curso JavaScript para Web - Manipulando o DOM

--- Front-end/JavaScript para Web - Manipulando o DOM/-robotron-2000-projeto_inicial/js/main.js
+++ Front-end/JavaScript para Web - Manipulando o DOM/-robotron-2000-projeto_inicial/js/main.js
@@ -1,10 +1,53 @@
-const robotron = document.querySelector("#robotron")
+// const robotron = document.querySelector("#robotron")
 
-robotron.addEventListener("click", (evento) => {
-    console.log(evento)
+// const controle = document.getElementsByClassName("controle-ajuste")
+// //const valorControle = controle.values()
+
+// const braco = document.querySelector("#bracos")
+// const somar = document.querySelector(".somar")
+// const subtrair = document.querySelector(".subtrair")
+
+
+// function manipulaDados(valor) {
+//     if (valor === "somar") {
+//         braco.value = parseInt(bracos.value) + 1
+//     }
+//     else {
+//         if (parseInt(bracos.value) > 0)
+//             braco.value = parseInt(bracos.value) - 1
+//     }
+// }
+// somar.addEventListener("click", () => {
+//     manipulaDados(somar)
+// })
+
+// subtrair.addEventListener("click", () => {
+//     manipulaDados(subtrair)
+// })
+//robotron.addEventListener("click", () => {
+//    console.log("oi")
+//})
+
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+
+
+function adicionarValor(parametro) {
+    if (parametro === "somar") {
+        console.log(parametro)
+        braco.value = parseInt(braco.value) + 1;
+    }
+    else {
+        braco.value = parseInt(braco.value) - 1;
+    }
+}
+
+somar.addEventListener("click", () => {
+    adicionarValor(somar)
+    console.log(somar)
 })
 
-function dizOi() {
-    console.log("oi")
-    console.log("Bem-vindo ao Robotron 2000")
-}
+subtrair.addEventListener("click", () => { adicionarValor(subtrair) })
+
+// algum erro na hr de acrecentar os valores
