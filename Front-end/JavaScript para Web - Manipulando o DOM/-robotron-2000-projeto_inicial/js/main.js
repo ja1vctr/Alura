faf7ccd04ce39b0e4beb174dc37190dac56fbaf4
joaovictor <jaovictor3690@gmail.com>
2023-03-23 17:59:46 -0300

    Ajustando paramentro da função adicionaValor dentro da função de seta

--- Front-end/JavaScript para Web - Manipulando o DOM/-robotron-2000-projeto_inicial/js/main.js
+++ Front-end/JavaScript para Web - Manipulando o DOM/-robotron-2000-projeto_inicial/js/main.js
@@ -1,32 +1,8 @@
-// const robotron = document.querySelector("#robotron")
+const robotron = document.querySelector("#robotron")
 
-// const controle = document.getElementsByClassName("controle-ajuste")
-// //const valorControle = controle.values()
+const controleAjuste = document.querySelectorAll("controle-ajuste")
+const controleContador = document.querySelectorAll("controle-contador")
 
-// const braco = document.querySelector("#bracos")
-// const somar = document.querySelector(".somar")
-// const subtrair = document.querySelector(".subtrair")
-
-
-// function manipulaDados(valor) {
-//     if (valor === "somar") {
-//         braco.value = parseInt(bracos.value) + 1
-//     }
-//     else {
-//         if (parseInt(bracos.value) > 0)
-//             braco.value = parseInt(bracos.value) - 1
-//     }
-// }
-// somar.addEventListener("click", () => {
-//     manipulaDados(somar)
-// })
-
-// subtrair.addEventListener("click", () => {
-//     manipulaDados(subtrair)
-// })
-//robotron.addEventListener("click", () => {
-//    console.log("oi")
-//})
 
 const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
@@ -39,15 +15,11 @@ function adicionarValor(parametro) {
         braco.value = parseInt(braco.value) + 1;
     }
     else {
-        braco.value = parseInt(braco.value) - 1;
+        if (parseInt(braco.value) > 0)
+            braco.value = parseInt(braco.value) - 1;
     }
 }
 
-somar.addEventListener("click", () => {
-    adicionarValor(somar)
-    console.log(somar)
-})
+somar.addEventListener("click", () => { adicionarValor("somar") })
 
-subtrair.addEventListener("click", () => { adicionarValor(subtrair) })
-
-// algum erro na hr de acrecentar os valores
+subtrair.addEventListener("click", () => { adicionarValor("subtrair") })
