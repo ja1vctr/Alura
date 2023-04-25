3738d77437d66ea791ac18869c416ec2f736d49e
joaovictor <jaovictor3690@gmail.com>
2023-04-25 14:21:15 -0300

    adicionando estrutura data- no HTML  para manipula-lás pelo JS

--- Front-end/JavaScript para Web - Manipulando o DOM/-robotron-2000-projeto_inicial/js/main.js
+++ Front-end/JavaScript para Web - Manipulando o DOM/-robotron-2000-projeto_inicial/js/main.js
@@ -1,14 +1,48 @@
 const controle = document.querySelectorAll("[data-controle]");
+const estatistica = document.querySelectorAll("[data-estatistica]");
+const peca = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -4
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 43
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach((element) => {
     element.addEventListener("click", (event) => {
         const valor = event.target.dataset.controle;
+        const pecaClicada = event.target.dataset.peca;
         const peca = event.target.parentNode;
+        console.log(pecaClicada);
 
         manipulaDados(valor, peca);
-
+        atualizaEstatisticas();
     })
-
 });
 
 function manipulaDados(valor, controle) {
@@ -24,5 +58,9 @@ function manipulaDados(valor, controle) {
             peca.value = parseInt(peca.value) - 1;
             console.log(peca.valor)
         }
+
+}
+
+function atualizaEstatisticas(pecaClicada) {
 
 }
