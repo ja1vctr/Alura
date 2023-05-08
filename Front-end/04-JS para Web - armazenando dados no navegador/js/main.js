862f5bdbdd8ce9b30dd42d25dbf3f0b4b9ae5ede
João Victor Oliveira Carneiro <jaovictor3690@gmail.com>
2023-05-08 18:22:14 -0300

    finalizada aula 2 do curso 04 com alterações no código

--- Front-end/04-JS para Web - armazenando dados no navegador/js/main.js
+++ Front-end/04-JS para Web - armazenando dados no navegador/js/main.js
@@ -1,27 +1,40 @@
-const form = document.getElementById("novoItem")
-const lista = document.getElementById("lista")
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const listaItens = [];
 
-form.addEventListener("submit", (evento)=>{
-    evento.preventDefault()
-    const nome       = evento.target.elements["nome"].value
-    const quantidade = evento.target.elements["quantidade"].value
-    // console.log(evento)
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault();
 
-    criaElemento(nome, quantidade)
-})
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
 
-function criaElemento(nome, quantidade){
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
-    
-    const quantidadeItem = document.createElement("strong")
-    quantidadeItem.innerHTML = quantidade
-    
-    novoItem.appendChild(quantidadeItem)
-    novoItem.innerHTML += nome
-    console.log(novoItem)
-    
-    console.log(document.getElementById("lista").lastChild)
-    lista.appendChild(novoItem)
-    console.log(lista)
+  criaElemento(nome.value, quantidade.value);
+
+  nome.value = "";
+  quantidade.value = "";
+});
+
+function criaElemento(nome, quantidade) {
+  if (nome === "" || quantidade === "") {
+  } else {
+    const novoItem = document.createElement("li");
+    novoItem.classList.add("item");
+
+    const quantidadeItem = document.createElement("strong");
+    quantidadeItem.innerHTML = quantidade;
+
+    novoItem.appendChild(quantidadeItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem);
+
+    itemAtual = {
+      nome: nome,
+      quantidade: quantidade,
+    };
+
+    listaItens.push(itemAtual);
+
+    localStorage.setItem("item", JSON.stringify(listaItens));
+  }
 }
