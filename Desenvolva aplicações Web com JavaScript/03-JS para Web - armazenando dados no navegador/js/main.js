65fefcc5d682b838e0b86983d6906e126e195851
joaovictor <jaovictor3690@gmail.com>
2023-05-17 11:46:53 -0300

    Corrigindo erro no ultimo commit do curso

--- Desenvolva aplicações Web com JavaScript/03-JS para Web - armazenando dados no navegador/js/main.js
+++ Desenvolva aplicações Web com JavaScript/03-JS para Web - armazenando dados no navegador/js/main.js
@@ -3,10 +3,9 @@ const lista = document.getElementById("lista");
 const listaItens = JSON.parse(localStorage.getItem("itens")) || [];
 
 // localStorage.clear();
-// console.log(listaItens);
 
 listaItens.forEach((element) => {
-  criaElemento(element);
+  criarElemento(element);
 });
 
 form.addEventListener("submit", (evento) => {
@@ -15,50 +14,85 @@ form.addEventListener("submit", (evento) => {
   const quantidade = evento.target.elements["quantidade"];
 
   const item = {
-    nome: no,
-    quantidade: quantidade,
+    nome: nome.value,
+    quantidade: quantidade.value,
   };
 
-  adicionaItem(item);
-
-  item.nome = "";
-  item.quantidade = "";
+  verificaExistencia(item);
 });
 
-function criaElemento(item) {
-  if (item.nome === "" || item.quantidade === "") {
+function criarElemento(item) {
+  const novoElemento = document.createElement("li");
+  const filhoNovoElemento = document.createElement("strong");
+
+  novoElemento.appendChild(filhoNovoElemento);
+  filhoNovoElemento.innerHTML = item.quantidade;
+
+  novoElemento.classList.add("item");
+  novoElemento.dataset.id = item.id;
+
+  novoElemento.innerHTML += item.nome;
+  novoElemento.appendChild(botaoDeleta(item.id));
+
+  lista.appendChild(novoElemento);
+
+  if (listaItens.find((element) => element.nome === item.nome)) {
   } else {
-    const novoItem = document.createElement("li");
-    novoItem.classList.add("item");
-
-    const quantidadeItem = document.createElement("strong");
-    quantidadeItem.innerHTML = quantidade;
-
-    novoItem.appendChild(quantidadeItem);
-    novoItem.innerHTML += nome;
-    novoItem.dataset.id = item.id;
-
-    lista.appendChild(novoItem);
+    listaItens.push(item);
   }
+
+  nome = "";
+  quantidade = "";
 }
 
-function adicionaItem(item) {
-  if (verificaExistente(item)) {
+function verificaExistencia(item) {
+  console.log(item);
+  const existe = listaItens.find((element) => item.nome === element.nome);
+
+  if (existe) {
+    item.id = existe.id;
+
+    const elemento = document.querySelector('[data-id="' + existe.id + '"]');
+    elemento.firstChild.innerHTML = item.quantidade;
+
+    listaItens[existe.id] = item;
+    localStorage.setItem("itens", JSON.stringify(listaItens));
   } else {
-    criaElemento(item);
+    item.id = listaItens.length;
 
-    listaItens.push(item);
-
+    criarElemento(item);
     localStorage.setItem("itens", JSON.stringify(listaItens));
   }
 }
 
-function verificaExistente(item) {
-  const existe = listaItens.find((elemento) => elemento.nome === item.nome);
-  console.log(existe);
-  if (existe) {
-    return true;
-  } else {
-    return false;
+function botaoDeleta(id) {
+  const elementoBotaoApaga = document.createElement("button");
+  elementoBotaoApaga.innerText = "X";
+  elementoBotaoApaga.classList.add("botao");
+
+  elementoBotaoApaga.addEventListener("click", function (event) {
+    //outra maneira de pegar o evento (dúvida)
+    //console.log(event.target);
+    console.log(document.querySelector('[data-id="' + id + '"]'));
+    deletaElemento(this.parentNode, id);
+  });
+
+  return elementoBotaoApaga;
+}
+
+function deletaElemento(tag, id) {
+  tag.remove();
+  listaItens.splice(
+    listaItens.findIndex((elemento) => elemento.id === id),
+    1
+  );
+  lista;
+  alteraIds(listaItens);
+  localStorage.setItem("itens", JSON.stringify(listaItens));
+}
+
+function alteraIds(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i].id = i;
   }
 }
