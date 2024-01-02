af2d9952194ba136d8e565c2e4dc19a47a9b8812
ja1vctr <jaovictor3690@gmail.com>
2024-01-01 23:56:40 -0300

    Adicionando async e await - aula 3

--- Desenvolva aplicações Web com JavaScript/05-JS para Web - consumindo e tratando dados de uma API/projeto-vidflow-main/script.js
+++ Desenvolva aplicações Web com JavaScript/05-JS para Web - consumindo e tratando dados de uma API/projeto-vidflow-main/script.js
@@ -1,25 +1,20 @@
 const containerVideos = document.querySelector('.videos__container')
 
-const api = fetch('http://localhost:3000/videos')
-  .then((res) =>
-    res.json().then((videos) =>
-      videos.forEach((element) => {
-        containerVideos.innerHTML += `
+async function buscarVideos() {
+  const busca = await fetch('http://localhost:3000/videos')
+  const videos = await busca.json
+
+  videos.forEach((element) => {
+    containerVideos.innerHTML += `
       <li class="videos__item">
-        <iframe src="${element.url}" title="${element.titulo}" frameborder="0" allowfullscreen></iframe>
-        
-        <div class="descricao-video">
-          <img class="img-canal" src="${element.imagem}" alt="Logo do Canal">
-          <h3 class="titulo-video">${element.titulo}</h3>
-          <p class="titulo-canal">${element.descricao}</p> 
-        </div>
+      <iframe src="${element.url}" title="${element.titulo}" frameborder="0" allowfullscreen></iframe>
+    
+      <div class="descricao-video">
+      <img class="img-canal" src="${element.imagem}" alt="Logo do Canal">
+      <h3 class="titulo-video">${element.titulo}</h3>
+      <p class="titulo-canal">${element.descricao}</p> 
+      </div>
       </li>
       `
-      })
-    )
-  )
-  .catch((error) => {
-    containerVideos.innerHTML = `
-  <p>Houve um erro ao carregar os vídeos: ${error}</p>
-  `
   })
+}
