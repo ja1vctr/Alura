5e217577b4976100e9e57ec78f1c751d79ca3f9c
ja1vctr <jaovictor3690@gmail.com>
2024-01-02 00:07:36 -0300

    Usando try catch e throw new Error - aula 3

--- Desenvolva aplicações Web com JavaScript/05-JS para Web - consumindo e tratando dados de uma API/projeto-vidflow-main/script.js
+++ Desenvolva aplicações Web com JavaScript/05-JS para Web - consumindo e tratando dados de uma API/projeto-vidflow-main/script.js
@@ -1,11 +1,15 @@
 const containerVideos = document.querySelector('.videos__container')
 
 async function buscarVideos() {
-  const busca = await fetch('http://localhost:3000/videos')
-  const videos = await busca.json
+  try {
+    const busca = await fetch('http://localhost:3000/videos')
+    const videos = await busca.json()
 
-  videos.forEach((element) => {
-    containerVideos.innerHTML += `
+    videos.forEach((element) => {
+      if (element.categoria == '') {
+        throw new Error('Vídeo sem categoria!')
+      }
+      containerVideos.innerHTML += `
       <li class="videos__item">
       <iframe src="${element.url}" title="${element.titulo}" frameborder="0" allowfullscreen></iframe>
     
@@ -16,5 +20,10 @@ async function buscarVideos() {
       </div>
       </li>
       `
-  })
+    })
+  } catch (error) {
+    containerVideos.innerHTML = `<p>Ocorreu um erro ao carregar os vídeos: ${error}</p>`
+  }
 }
+
+buscarVideos()
