81fd3e39ffcac980c884b29995f4060e0b3ceae4
ja1vctr <jaovictor3690@gmail.com>
2024-01-19 13:08:19 -0300

    Primeiro uso de states (exemplo) - aula 5

--- Curos - React escrevendo com Typescript/alura-studies/src/components/list/index.tsx
+++ Curos - React escrevendo com Typescript/alura-studies/src/components/list/index.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import ItemLista from './itens'
 import style from './list.module.scss'
 
 export default function List() {
-  const listaTarefas = [
+  const [texto, setTexto] = useState('Meu estado')
+  const [listaTarefas, setListaTarefas] = useState([
     {
       tarefa: 'React',
       tempo: '01:30:00',
@@ -19,13 +21,22 @@ export default function List() {
       tarefa: 'Vue.js',
       tempo: '02:45:00',
     },
-  ]
+  ])
   return (
     <aside className={style.listaTarefas}>
-      <h2>Lista de estudos</h2>
+      <h2
+        onClick={() => {
+          setListaTarefas([
+            ...listaTarefas,
+            { tarefa: 'MANIFESTO DO PC', tempo: '05:00:00' },
+          ])
+        }}
+      >
+        Lista de estudos
+      </h2>
       <ul>
         {listaTarefas.map((item, index) => (
-          <ItemLista key="index" {...item} />
+          <ItemLista key={index} {...item} />
         ))}
       </ul>
     </aside>
