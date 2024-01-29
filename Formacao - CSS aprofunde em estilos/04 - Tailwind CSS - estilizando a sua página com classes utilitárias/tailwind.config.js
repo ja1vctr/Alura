f71140233300009ec116a4e480fa977f998d37c6
ja1vctr <jaovictor3690@gmail.com>
2024-01-29 14:01:01 -0300

    aidcionando arquivos da pagina newsletter.html - aula 2

--- Formacao - CSS aprofunde em estilos/04 - Tailwind CSS - estilizando a sua página com classes utilitárias/tailwind.config.js
+++ Formacao - CSS aprofunde em estilos/04 - Tailwind CSS - estilizando a sua página com classes utilitárias/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    colors: {
+      extend: {
+        azul: {
+          claro: '#C5DFFF',
+          escuro: '#061E3C',
+          hover: '#1057B0',
+        },
+      },
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+    },
   },
   plugins: [],
 }
